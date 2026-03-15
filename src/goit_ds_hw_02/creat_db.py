8227ddf4fdedb5d3212b18d509c08f9220d3f886
Евгений Лысенко <udjiedwards@gmail.com>
2026-03-16 00:37:45 +0200

    new HM goit-ds-hw-02

---
+++ src/goit_ds_hw_02/creat_db.py
@@ -0,0 +1,23 @@
+import sqlite3
+from pathlib import Path
+
+# use pathlib to get the directory of the current script
+base_dir = Path(__file__).parent
+
+sql_path = base_dir / "create_table.sql"
+db_path = base_dir / "user_tasks.db"
+
+def create_db():
+      # read the SQL script from the file for creating the database
+      with open(sql_path, "r") as f:
+        sql = f.read()
+
+      # create a connection to the database (if the database file doesn't exist, it will be created)
+      with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        # execute the SQL script
+        cur.executescript(sql)
+
+if __name__ == "__main__":
+  create_db()
+  print("Database created")
---
+++ src/goit_ds_hw_02/creat_table.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS status;
+
+
+-- Table: users
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fullname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+    );
+
+-- Table: status
+CREATE TABLE status (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+INSERT INTO status (name) VALUES
+('new'),
+('in progress'),
+('completed');
+
+-- Table: tasks
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    status_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (status_id) REFERENCES status (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
